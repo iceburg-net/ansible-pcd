35ac561255cecbb6b8b7db77829bfd4619a6daab
Brice Burgess <briceburg@gmail.com>
2014-04-22 10:03:21 -0500

    surpress cron emails with cronic

--- roles/applications/awstats/templates/prerotate.sh
+++ roles/applications/awstats/templates/prerotate.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #sudo -u {{ apache_sites_user }} {{ awstats_site_dir }}/awstats_updateall.pl now
-{{ awstats_site_dir }}/awstats_updateall.pl now
+{% if cronic_cron %}cronic {% endif %}{{ awstats_site_dir }}/awstats_updateall.pl now
